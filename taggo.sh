16cf80b6962d46081b3a847fb975f1b45d3c252b
yuanxiaolin <yuanxiaolin.pm@gmail.com>
2017-04-14 17:47:50 +0900

    taggo shell script add

---
+++ taggo.sh
@@ -0,0 +1,47 @@
+# ctagsとcscope用のtagファイルを作成する
+
+# 除外するキーワードを設定する
+if [ $# -eq 0 ]; then
+    EXCLUDEFILE="test|mock|bak_IP10|Work"
+elif [ "$1" = "all" ]; then
+    EXCLUDEFILE=""
+else
+    EXCLUDEFILE=$1
+fi
+    
+echo "$EXCLUDEFILE を除外する"
+
+# 確認
+read -p "$PWDでtagファイルを作成する。Y/N:" result
+
+if [ $result = "Y" ]; then
+    : #何もせず
+elif [ $result = "N" ]; then
+    echo "終了しました。"
+    exit
+else
+    echo "無効値です！"
+    exit
+fi
+
+# taglist
+
+# remove tag.file first
+if [ -e tag.file ]; then
+    #存在する場合
+    rm -f tag.file
+    echo "tag.file deleted"
+fi
+
+# create new tag.file
+find . -type f -print | grep -E '\.(c(pp)?|h)$' | grep -v -E ${EXCLUDEFILE} > tag.file
+echo "tag.file created"
+
+# cscope
+cscope -b -i tag.file
+echo "cscope tag created"
+
+# ctaggo
+ctags -L tag.file
+echo "ctags tag created"
+
